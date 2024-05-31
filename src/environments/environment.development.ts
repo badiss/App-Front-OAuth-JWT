3b72c8de0355dacee56f36ae4de330150a4640ed
HajerRe <hajer.rejeb@labom2iformation.fr>
2024-05-31 15:42:30 +0200

    modifier les params environnement

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    //backendHost : "http://localhost:8021/api",
-    backendHost : "http://192.168.76.3:8021/api",
+    backendHost : "http://localhost:8021/api",
+    //backendHost : "http://192.168.76.3:8021/api",
     production : false
 };
